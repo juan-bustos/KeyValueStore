f96a91386e283887d496308579d8856d7b69a4cb
Juan Bustos <juan.c.bustos@outlook.com>
2019-09-14 13:25:48 -0700

    Add project files.

---
+++ KeyValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyValueStore
+{
+    public struct KeyValue
+    {
+        public readonly string key;
+        public readonly object value;
+        public KeyValue(string setKey, object setValue)
+        {
+            key = setKey;
+            value = setValue;
+        }
+    }
+}
---
+++ MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyValueStore
+{
+    class MyDictionary
+    {
+        KeyValue[] keyValueArr = new KeyValue[10];
+        private int storedValues;
+
+        public object this[string key]
+        {
+            get
+            {
+                for (int i = 0; i < keyValueArr.Length; i++)
+                {
+                    if (keyValueArr[i].key == key)
+                        return keyValueArr[i].value;
+                }
+                throw new KeyNotFoundException(key);
+            }
+            set
+            {
+                for (int i = 0; i < keyValueArr.Length; i++)
+                {
+                    if (keyValueArr[i].key == key)
+                    {
+                        keyValueArr[i] = new KeyValue(key, value);
+                        return;
+                    }
+                    if (keyValueArr[i].key == null)
+                    {
+                        keyValueArr[i] = new KeyValue(key, value);
+                        storedValues++;
+                        return;
+                    }
+                }
+            }             
+        }
+    }
+}
